9fe4a36fa6c6104d7a3320a5c2dca2e227cbd0ca
Maxi Clayton Clowes <maxi@codogo.io>
2018-03-15 17:23:44 +0000

    v1

---
+++ components/Nav/Links.js
@@ -0,0 +1,133 @@
+import React from "react";
+import styled, { css, } from "styled-components";
+
+import {
+	bp,
+	shadow,
+	xs,
+	transparent,
+	clearfix,
+	bpEither,
+} from "codogo-mixins";
+
+// --------------------------------------------------
+
+const linksStyle = [
+	css`
+		align-items: center;
+		background: ${ props => props.backgroundColor.xs };
+		left: 0;
+		position: absolute;
+		right: 0;
+		top: ${ props => props.topOffset.xs };
+		transform: translateY(${ props => ( props.open ? 0 : -110 ) }%);
+		transition: 0.3s all ease-out;
+		${ shadow(2) };
+	`,
+	css`
+		position: absolute;
+		right: 0;
+		top: 0;
+		bottom: 0;
+		display: flex;
+		align-items: center;
+	`,
+];
+
+const LinksWrapper = styled.div`
+	${ xs`${ linksStyle[0] }` };
+	${ bp.sm.min`${ linksStyle[1] }` };
+`;
+
+const linkStyle = [
+	css`
+		display: block;
+		padding: ${ props => props.margin.xs };
+		border-bottom: 1px solid ${ transparent(0.1) };
+		color: ${ props => props.color.xs };
+	`,
+	css`
+		line-height: ${ props => props.height.other };
+		padding: 0 ${ props => props.margin.other };
+		letter-spacing: 0.1em;
+		color: ${ props => props.color.other };
+
+		&:hover {
+			text-decoration: underline;
+		}
+	`,
+];
+
+const Link = styled.div`
+	text-transform: ${ props => props.textTransform };
+	font-size: 1.1em;
+
+	&.active {
+		font-weight: bold;
+		opacity: 0.7;
+	}
+
+	a {
+		${ clearfix.link };
+		${ props => bpEither("color", props.color ) };
+	}
+
+	${ xs`${ linkStyle[0] }` };
+	${ bp.sm.min`${ linkStyle[1] }` };
+`;
+
+// --------------------------------------------------
+
+const Links = props => (
+	<LinksWrapper 
+		open = { props.open } 
+		backgroundColor = { props.backgroundColor }
+		topOffset = { props.topOffset }
+	>
+		{ 
+			props.children.map(
+				(navlink, i) => 
+				<Link 
+					key = { 'navlink' + i }
+					{ ...props }
+				>
+					{ navlink }
+				</Link>
+			) 
+		}
+	</LinksWrapper>
+);
+
+Links.defaultProps = {
+	textTransform: {
+		xs: "uppercase",
+		other: "uppercase",
+	},
+	topOffset: {
+		xs: "50px",
+		other: "70px",
+	},
+	padding: {
+		xs: "20px",
+		other: "30px",
+	},
+	margin: {
+		xs: "20px",
+		other: "30px",
+	},
+	height: {
+		xs: "50px",
+		other: "30px",
+	},
+	color: {
+		xs: "#fff",
+		other: "#fff",
+	},
+	backgroundColor: {
+		xs: "#333",
+		other: "#333",
+	},
+};
+
+
+export default Links
---
+++ components/Nav/index.js
@@ -0,0 +1,158 @@
+import React from "react";
+import styled from "styled-components";
+
+import Links from "./Links";
+import Burger from "./Burger";
+import Fade from "./Fade";
+
+import {
+	bp,
+	bpEither,
+	contained,
+	shadow,
+	sm,
+	transparent,
+} from "codogo-mixins";
+
+// --------------------------------------------------
+
+const NavContainer = styled.div`
+	${ props => bpEither("height", props.height ) };
+`;
+
+const NavWrapper = styled.nav`
+	left: 0;
+	position: absolute;
+	right: 0;
+	top: 0;
+	z-index: 5;
+
+	${ bp.sm.min`${ shadow(0) }` };
+	${ props => bpEither("background-color", props.backgroundColor ) };
+	${ props => bpEither("height", props.height ) };
+
+	${ sm`padding: 0 1em; ` };
+	${ bp.md.min`padding: 0 3em; ` };
+`;
+
+const NavInner = styled.div`
+	width: 100%;
+	height: 100%;
+	position: relative;
+`;
+
+const MobileContent = styled.div`
+	${ bp.sm.min`display: none;` };
+	${ contained() };
+`;
+
+const Dark = styled.div`
+	background: ${ transparent(0.5) };
+	position: fixed;
+	${ contained() };
+`;
+
+const Overlay = styled.div`
+	${ contained() };
+	${ ({ open, }) => (open ? shadow(1) : "") };
+	transition: 0.3s all ease-out;
+	${ props => bpEither("background-color", props.backgroundColor ) };
+`;
+
+const BurgerWrapper = styled.div`
+	position: absolute;
+	right: 0;
+	top: 50%;
+	margin-top: -20px;
+`;
+
+// -------------------------------
+
+export default class Nav extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			open: false,
+		};
+	}
+
+	render() {
+		return (
+			<NavContainer
+				height = { this.props.height }
+			>
+				<NavWrapper
+					height = { this.props.height }
+					backgroundColor = { this.props.backgroundColor }
+				>
+					<NavInner>
+						<MobileContent>
+							<Fade visible = { this.state.open }>
+								<Dark
+									onClick = { () =>
+										this.setState({
+											open: false,
+										})
+									}
+								/>
+							</Fade>
+						</MobileContent>
+
+						<Links
+							close = { () =>
+								this.setState({
+									open: false,
+								})
+							}
+							{ ...this.state }
+						>
+							{ this.props.children }
+						</Links>
+
+						<MobileContent>
+							<Overlay 
+								backgroundColor = { this.props.backgroundColor }
+								{ ...this.state } 
+							/>
+
+							<BurgerWrapper
+								onClick = { () =>
+									this.setState({
+										open: !this.state.open,
+									})
+								}
+							>
+								<Burger
+									color = { this.props.color }
+									backgroundColor = { this.props.backgroundColor }
+									{ ...this.state }
+								/>
+							</BurgerWrapper>
+						</MobileContent>
+
+						{ this.props.logo }
+					</NavInner>
+				</NavWrapper>
+			</NavContainer>
+		);
+	}
+}
+
+Nav.defaultProps = {
+	padding: {
+		xs: "30px",
+		other: "30px",
+	},
+	color: {
+		xs: "#fff",
+		other: "#fff",
+	},
+	backgroundColor: {
+		xs: "#333",
+		other: "#333",
+	},
+	height: {
+		xs: "50px",
+		other: "70px",
+	},
+};
